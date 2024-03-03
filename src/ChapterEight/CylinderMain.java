cde6efec5327806ec7f8273cd23cf507411ab5fc
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-03 11:33:44 +0100

    update

---
+++ src/ChapterEight/CylinderMain.java
@@ -0,0 +1,32 @@
+package ChapterEight;
+
+import ChapterEight.Cylinder;
+
+import java.util.Scanner;
+
+public class CylinderMain {
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+
+        Cylinder cylinder = new Cylinder();
+
+        System.out.println("Enter radius:");
+        int radius = input.nextInt();
+
+        System.out.println("Enter height:");
+        int height = input.nextInt();
+
+        try {
+            cylinder.setRadius(radius);
+            cylinder.setHeight(height);
+        }
+        catch (IllegalArgumentException e){
+            System.out.printf("Exception while initialising radius: %s%n", e.getMessage());
+        }
+
+        System.out.printf("%nVolume: %.2f", cylinder.cylinderVolume());
+
+        input.close();
+    }
+
+}
