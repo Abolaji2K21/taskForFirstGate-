f9a0c9f222767b26fe752d02e60c769bc6f495f6
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-07 05:17:28 +0100

    Commit

--- src/Chapter15/Person.java
+++ src/Chapter15/Person.java
@@ -1,25 +1,36 @@
 package Chapter15;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+//import com.fasterxml.jackson.
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Person {
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String name;
-    @JsonProperty("DOB")
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String dateOfBirth;
+//    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+//        @JsonProperty("DOB")
+@JsonSerialize(using = LocalDateSerializer.class)
+@JsonDeserialize(using = LocalDateDeserializer.class)
+private LocalDate dateOfBirth;
 
 
 
 
-    public String getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
--- test/Chapter15/JsonSerializationTest.java
+++ test/Chapter15/JsonSerializationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.LoggerFactory;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,7 +16,7 @@ class JsonSerializationTest {
     public void testSerialization() throws JsonProcessingException {
         Person person = new Person();
         person.setName("BEEJAY");
-        String dateOfBir = "71-02-1876";
+        LocalDate dateOfBir = LocalDate.of(2024,5,6);
         person.setDateOfBirth(dateOfBir);
         person.setGender(Gender.FEMALE);
         person.setPhoneNumber("08165269244");
@@ -23,7 +24,7 @@ class JsonSerializationTest {
 
         String json = JsonSerialization.serilization(person);
         System.out.println(json);
-        String expected ="{\"name\":\"BEEJAY\",\"dateOfBirth\":\"71-02-1876\",\"phoneNumber\":\"08165269244\",\"gender\":\"FEMALE\"}";
+        String expected ="{\"name\":\"BEEJAY\",\"dateOfBirth\":[2024,5,6],\"phoneNumber\":\"08165269244\",\"gender\":\"FEMALE\"}";
         assertEquals(expected,json);
     }
 
@@ -31,13 +32,13 @@ class JsonSerializationTest {
     void testDeSerialization() throws JsonProcessingException {
         Person person = new Person();
         person.setName("BEEJAY");
-        String dateOfBir = "71-02-1876";
+        LocalDate dateOfBir = LocalDate.of(2024,5,6);
         person.setDateOfBirth(dateOfBir);
         person.setGender(Gender.FEMALE);
         person.setPhoneNumber("08165269244");
 
-        String json = "{\"name\":\"BEEJAY\",\"dateOfBirth\":\"71-02-1876\",\"phoneNumber\":\"08165269244\",\"gender\":\"FEMALE\"}";
-//        System.out.println("json" + json);
+        String json = JsonSerialization.serilization(person);
+
         Person personFromJson = JsonSerialization.deserialize(json);
         System.out.println("person from json:" + personFromJson);
 
