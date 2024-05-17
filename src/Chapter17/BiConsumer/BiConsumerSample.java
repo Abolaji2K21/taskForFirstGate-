99cb378c01bc6432716d72f59ef577187983f257
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-17 11:57:23 +0100

    Awesome

---
+++ src/Chapter17/BiConsumer/BiConsumerSample.java
@@ -0,0 +1,18 @@
+package Chapter17.BiConsumer;
+
+import java.util.NavigableMap;
+import java.util.function.BiConsumer;
+
+public class BiConsumerSample {
+    public static void main(String[] args) {
+        BiConsumer<String, Integer> biConsumer = (Name, Age)->{
+            printNameAndAge(Name,Age);
+        };
+        biConsumer.accept("BeeJay",30);
+    }
+
+    private static void printNameAndAge(String Name, int Age){
+        System.out.println("You have entered: "+ Name);
+        System.out.println("And your Age Is: "+ Age);
+    }
+}
