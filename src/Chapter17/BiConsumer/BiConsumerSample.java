0e3987a08dc1737aae3778920146b4126ec1aff3
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-17 12:00:51 +0100

    Awesome

--- src/Chapter17/BiConsumer/BiConsumerSample.java
+++ src/Chapter17/BiConsumer/BiConsumerSample.java
@@ -5,12 +5,9 @@ import java.util.function.BiConsumer;
 
 public class BiConsumerSample {
     public static void main(String[] args) {
-        BiConsumer<String, Integer> biConsumer = (Name, Age)->{
-            printNameAndAge(Name,Age);
-        };
+        BiConsumer<String, Integer> biConsumer = (Name, Age)->{printNameAndAge(Name,Age);};
         biConsumer.accept("BeeJay",30);
     }
-
     private static void printNameAndAge(String Name, int Age){
         System.out.println("You have entered: "+ Name);
         System.out.println("And your Age Is: "+ Age);
