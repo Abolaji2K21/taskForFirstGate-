e34b4c5222deefdc943d89a61b06b01117ca9c66
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-17 15:07:53 +0100

    Ohokay

--- src/Chapter17/BiConsumer/BiConsumerSample.java
+++ src/Chapter17/BiConsumer/BiConsumerSample.java
@@ -1,5 +1,6 @@
 package Chapter17.BiConsumer;
 
+import java.util.Map;
 import java.util.NavigableMap;
 import java.util.function.BiConsumer;
 
@@ -7,6 +8,13 @@ public class BiConsumerSample {
     public static void main(String[] args) {
         BiConsumer<String, Integer> biConsumer = (Name, Age)->{printNameAndAge(Name,Age);};
         biConsumer.accept("BeeJay",30);
+
+        Map<String, Integer> map = Map.of(
+                "John", 10,
+                "Joe", 20,
+                "Josh", 32
+        );
+        map.forEach(biConsumer);
     }
     private static void printNameAndAge(String Name, int Age){
         System.out.println("You have entered: "+ Name);
--- src/Chapter17/Supplier/SupplierSample.java
+++ src/Chapter17/Supplier/SupplierSample.java
@@ -4,11 +4,13 @@ import Chapter16.Transactions;
 
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class SupplierSample {
     public static void main(String[] args) {
         Supplier<Integer> supplier = ()-> new Random().nextInt();
         System.out.println(supplier.get());
+        Stream.generate(supplier).forEach( x-> System.out.println(x));
     }
     Supplier<Transactions> transactionsSupplier = () -> new Transactions();
 }
