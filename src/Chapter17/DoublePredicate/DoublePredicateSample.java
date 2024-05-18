a61b06ec726523d59071efb7d7f215216f0b32cf
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-18 12:53:32 +0100

    Awesome

---
+++ src/Chapter17/DoublePredicate/DoublePredicateSample.java
@@ -0,0 +1,15 @@
+package Chapter17.DoublePredicate;
+
+import java.util.function.DoublePredicate;
+
+public class DoublePredicateSample {
+    public static void main(String[] args) {
+        DoublePredicate doublePredicate = (FirstValue)-> {
+            return true;
+        };
+        double sampleValue = 3.0;
+        boolean result = doublePredicate.test(sampleValue);
+        System.out.println(result);
+
+    }
+}
