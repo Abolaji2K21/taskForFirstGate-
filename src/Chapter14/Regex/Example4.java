4aef98fa7e167f1aa2ac55a0ecdc94e2f1fd3fac
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 07:12:31 +0100

    PenIsActivated

---
+++ src/Chapter14/Regex/Example4.java
@@ -0,0 +1,15 @@
+package Chapter14.Regex;
+
+public class Example4  {
+    public static void main(String[] args) {
+        String regex = "^\\d+";
+
+        String number = "273536736";
+
+        if (!number.matches(regex)){
+            System.err.println("invalid number");
+            System.exit(1);
+        }
+        System.out.println(number);
+    }
+}
---
+++ src/Chapter14/example/Example1.java
@@ -0,0 +1,14 @@
+package Chapter14.example;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        String s1 = "JOHN"; //StringPool
+
+        String s2 = new String("john"); // Heap
+
+
+
+
+    }
+}
