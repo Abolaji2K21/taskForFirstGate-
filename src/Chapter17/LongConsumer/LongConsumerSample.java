87c382681a5202bb66e3e2728243a7d2cbfa497f
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 05:44:51 +0100

    OneLastPush

--- src/Chapter17/LongConsumer/LongConsumerSample.java
+++ src/Chapter17/LongConsumer/LongConsumerSample.java
@@ -1,7 +1,15 @@
 package Chapter17.LongConsumer;
 
+import java.util.function.LongConsumer;
+
 public class LongConsumerSample {
     public static void main(String[] args) {
 
+        LongConsumer printLong = (num) -> {
+            System.out.println(num);
+
+        };
+        long number = 42L;
+        printLong.accept(number);
     }
 }
