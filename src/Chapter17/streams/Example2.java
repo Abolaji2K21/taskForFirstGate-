5274b69dd28a17990d502561766bc90001bf62a9
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 15:47:50 +0100

    Oboi People they Write code

---
+++ src/Chapter17/streams/Example2.java
@@ -0,0 +1,11 @@
+package Chapter17.streams;
+
+import java.util.stream.IntStream;
+
+public class Example2 {
+        public static void main(String[] args) {
+            IntStream.range(1, 10)
+                    .filter((number) -> number % 2 == 0)
+                    .forEach(System.out::println);
+        }
+    }
---
+++ src/Chapter17/streams/Example3.java
@@ -0,0 +1,11 @@
+package Chapter17.streams;
+
+import java.util.List;
+
+public class Example3 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(10,20,30,40,50);
+
+        numbers.stream().filter();
+    }
+}
