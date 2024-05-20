c7a572f9f18cfbe416a548235c659c7803b80fb8
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 05:42:10 +0100

    Done

--- src/Chapter17/LongFunction/LongFunctionSample.java
+++ src/Chapter17/LongFunction/LongFunctionSample.java
@@ -1,7 +1,16 @@
 package Chapter17.LongFunction;
 
+import java.util.function.LongFunction;
+
 public class LongFunctionSample {
     public static void main(String[] args) {
+        LongFunction<String> longToBinary = (num) -> {
+            return Long.toBinaryString(num);
+        };
+
+        long number = 10L;
+        String binaryRepresentation = longToBinary.apply(number);
+        System.out.println(binaryRepresentation);
 
     }
 }
