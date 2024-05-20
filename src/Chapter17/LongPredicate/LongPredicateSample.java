c58f6a39b1ac673ed6388ae7bba7f7eff77ce942
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 05:40:00 +0100

    got an hr left

--- src/Chapter17/LongPredicate/LongPredicateSample.java
+++ src/Chapter17/LongPredicate/LongPredicateSample.java
@@ -1,7 +1,19 @@
 package Chapter17.LongPredicate;
 
+import java.util.function.LongPredicate;
+
 public class LongPredicateSample {
     public static void main(String[] args) {
+        LongPredicate isEven = (num) -> {
+            return num % 2 == 0;
+        };
 
+        long number = 10L;
+        if (isEven.test(number)) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is not even.");
+        }
     }
 }
+
