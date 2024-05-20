c84cfac858289278153ad04343b6afd8bd31c822
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 16:05:08 +0100

    ..

--- src/Chapter17/streams/Example3.java
+++ src/Chapter17/streams/Example3.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public class Example3 {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(10,20,30,40,50);
+        List<Integer> numbers = List.of(10,20,33,40,50);
 
         numbers.
                 stream().
                 filter((number)
-                        -> number % 2 == 0 );
+                        -> number % 2 != 0 )
+                .forEach(System.out::println);
     }
 }
---
+++ src/Chapter17/streams/Example4.java
@@ -0,0 +1,10 @@
+package Chapter17.streams;
+
+import java.util.List;
+
+public class Example4 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(10,20,33,40,50);
+
+    }
+}
