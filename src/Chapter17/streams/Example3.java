16ead4dd858b101fa023296d3df039bbdb5cdf92
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-20 15:56:35 +0100

    Okay Dokkey

--- src/Chapter17/streams/Example3.java
+++ src/Chapter17/streams/Example3.java
@@ -6,6 +6,9 @@ public class Example3 {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(10,20,30,40,50);
 
-        numbers.stream().filter();
+        numbers.
+                stream().
+                filter((number)
+                        -> number % 2 == 0 );
     }
 }
